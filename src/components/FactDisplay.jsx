a6f4451119e3b472ac6041a182c1114a7b8a71a1
Louis Simeone <lsimeone@umich.edu>
2025-01-13 14:35:38 -0500

    Favicon

--- src/components/FactDisplay.jsx
+++ src/components/FactDisplay.jsx
@@ -1,4 +1,4 @@
-import React, { useState, useRef } from 'react';
+import React, { useState, useRef, useEffect } from 'react';
 import axios from 'axios';
 import './FactDisplay.css';
 const BASE_URL = process.env.REACT_APP_API_URL;
@@ -7,7 +7,24 @@ const FactDisplay = ({ articles, error, isLoading }) => {
   const [expandedArticleIndex, setExpandedArticleIndex] = useState(null);
   const [additionalData, setAdditionalData] = useState({});
   const [loadingAdditionalData, setLoadingAdditionalData] = useState(false);
+  const [topHeadlines, setTopHeadlines] = useState([]);
+  const [loadingTopHeadlines, setLoadingTopHeadlines] = useState(true);
   const cache = useRef({});
+
+  useEffect(() => {
+    const fetchTopHeadlines = async () => {
+      try {
+        const response = await axios.get(`${BASE_URL}/top-headlines`);
+        setTopHeadlines(response.data.articles);
+      } catch (err) {
+        console.error('Error fetching top headlines:', err);
+      } finally {
+        setLoadingTopHeadlines(false);
+      }
+    };
+
+    fetchTopHeadlines();
+  }, []);
 
   const handleToggleExpand = async (index, article) => {
     if (expandedArticleIndex === index) {
@@ -45,12 +62,32 @@ const FactDisplay = ({ articles, error, isLoading }) => {
     }
   };
 
+  if (loadingTopHeadlines) {
+    return <div className="no-facts">Loading top headlines...</div>;
+  }
+
   if (error) {
     return <div className="error">Error: {error}</div>;
   }
 
   if ((!articles || articles.length === 0) && !isLoading) {
-    return <div className="no-facts">Unbiased Insights, AI-Powered Clarity</div>;
+    return (
+      <div className="fact-display">
+        <h3>Top Headlines:</h3>
+        <ul>
+          {topHeadlines.map((article, index) => (
+            <li key={index} className="fact-item bullet-color" onClick={() => handleToggleExpand(index, article)}>
+              <h4>{article.title}</h4>
+              <p className='article-description'>{article.description}</p>
+              <a href={article.url} target="_blank" rel="noopener noreferrer">
+                Read full article
+              </a>
+              <p>Source: {article.source}</p>
+            </li>
+          ))}
+        </ul>
+      </div>
+    );
   }
 
   if (isLoading) {
